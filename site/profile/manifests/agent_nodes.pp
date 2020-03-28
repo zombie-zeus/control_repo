3cca0e54461b58fc6309850a8d11bdc566ff6064
zombie-zeus <36420541+zombie-zeus@users.noreply.github.com>
2020-03-28 13:39:31 +0430

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,14 @@
 class profile::agent_nodes {
-  include docker
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
+  include dockeragent
+  dockeragent::node { 'web.puppet.vm': }
+  dockeragent::node { 'db.puppet.vm': }
+  dockeragent::node { 'minetest.puppet.vm': }
+  host {‘web.puppet.vm’:
+    ensure => present,
+    ip => ‘172.18.0.2’,
+  }
+  host {‘db.puppet.vm’:
+    ensure => present,
+    ip => ‘172.18.0.3’,
+  }
 }
