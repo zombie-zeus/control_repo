509801201c283625d398c64f290b2e5322ef72cf
zombie-zeus <36420541+zombie-zeus@users.noreply.github.com>
2020-04-05 17:02:37 +0430

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,11 @@
+node default {
+}
 node 'master.puppet.vm' {
   include role::master_server
+  file { '/root/README':
+    ensure    => file,
+    content   => $fqdn,
+  }
 }
 node /^web/ { 
   include role::app_server
