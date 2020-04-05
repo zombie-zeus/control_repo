b3f4dff2d2bbb9026a36e3b83dba8b19a33d721a
zombie-zeus <36420541+zombie-zeus@users.noreply.github.com>
2020-04-05 13:53:27 +0430

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,5 @@
-node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
-}
 node 'master.puppet.vm' {
   include role::master_server
-  file {'/root/README':
-    ensure => file,
-    #content => 'Welcome to ${fqdn}',
-    owner => 'root',
   }
 }
 node /^web/ { 
