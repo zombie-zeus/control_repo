0314ad453e5dfcb65788f3bbd8a485e706902d01
zombie-zeus <36420541+zombie-zeus@users.noreply.github.com>
2020-04-05 17:30:42 +0430

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCXpSbAvl/A1oB06v0V474mYh1dip/JO19h3Uc0PSLaIJUSJ3Jr41EdAdGpWWVIw47RdZsVWZn0vhI2X40DBJCgW166RZV7sfbgQXnOgiRL+mpLwT3sKfDBsl4jk4Bc6BhvWTsgan8A59it7YaYt8dh6K/cXzs4Jhw0EUa21BAZrL1WReqq4T5+juqYiwEyYRT1A+HXYgxv0rgYqfma3BBATD1Np+cwx92VcwtcM2a07X3Ig9oV7GNfjX2yE+3KYi0krbNT6Nm3j58zB13/oA+0EKwiiEaaIsTY3voGIPTWYFZrgnkYrppr9jfw7u3x/SFrfRKmXAiWMcLkHNsiR103',
   }  
 }
